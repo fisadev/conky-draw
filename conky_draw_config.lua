593b18863eff1cc55b5d315a17d148b261c4be0e
Juan Pedro Fisanotti <fisadev@gmail.com>
2016-02-01 19:04:51 -0300

    Basic example elements

--- conky_draw_config.lua
+++ conky_draw_config.lua
@@ -4,4 +4,40 @@
 -- (and be sure to use the lastest version)
 
 elements = {
+    {
+        kind = 'line',
+        from = {x=200, y=200},
+        to = {x=300, y=400},
+
+        color = 0xFF00FF,
+        alpha = 1,
+        thickness = 3,
+    },
+    {
+        kind = 'ring',
+        center = {x=200, y=200},
+        radius = 50,
+
+        color = 0xFF00FF,
+        alpha = 1,
+        thickness = 3,
+
+        start_angle = 270,
+        end_angle = 90,
+    },
+    {
+        kind = 'bar_graph',
+        conky_value = 'cpu cpu0',
+        from = {x=200, y=200},
+        to = {x=300, y=300},
+    },
+    {
+        kind = 'ring_graph',
+        conky_value = 'cpu cpu0',
+        center = {x=200, y=200},
+        radius = 80,
+
+        start_angle = 0,
+        end_angle = 270,
+    },
 }
