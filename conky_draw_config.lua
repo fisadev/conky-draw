3a71d86449b9da4659f870c4a676dc89871fbc54
Juan Pedro Fisanotti <fisadev@gmail.com>
2016-02-05 19:55:29 -0300

    Better wording

--- conky_draw_config.lua
+++ conky_draw_config.lua
@@ -1,6 +1,6 @@
 -- Define your visual elements here
--- For a complete list on the possible elements and their properties, go
--- to https://github.com/fisadev/conky-draw/
+-- For examples, and a complete list on the possible elements and their 
+-- properties, go to https://github.com/fisadev/conky-draw/
 -- (and be sure to use the lastest version)
 
 elements = {
